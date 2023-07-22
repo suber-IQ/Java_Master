1bc6ab835bbea01a399223c8eba6051a848ee3bf
sumit <suberquercusumit@gmail.com>
2023-07-22 20:34:19 +0530

    example of array of java

---
+++ Arrays_in_Java/example-arrays.java
@@ -0,0 +1,34 @@
+package Arrays_in_Java;
+
+
+class ArrayDemo{
+      public static void main(String[] args){
+            // int age[];
+           // age = new int[5];
+          //int [] age = new int[5];
+      //     int age[] = new int[5];
+      
+      //       age[0] = 20;
+      //       age[1] = 29;
+      //       age[2] = 30;
+      //       age[3] = 35;
+      //       age[4] = 45;
+
+            // for(int i = 0; i < age.length; i++){
+            //       System.out.println(age[i]);
+            // }
+            // int sum = 0;
+            // for(int val:age){
+                  // System.out.println(val);
+                  // sum += val;
+            // }
+            // System.out.println(sum);
+
+            // int [] marks = {22,29,35,45,50};
+            int marks[] = {22,29,39,45,50};
+
+            for(int mark: marks){
+                  System.out.println(mark);
+            }
+      }
+}
