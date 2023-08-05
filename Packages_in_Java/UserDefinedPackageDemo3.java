3e8b6844ef79b5dbb9ff94df60dfc4e5314d581f
sumit <suberquercusumit@gmail.com>
2023-08-05 12:43:34 +0530

    completed lesson 2 of Packages in java with practical and readme file note create

---
+++ Packages_in_Java/UserDefinedPackageDemo3.java
@@ -0,0 +1,9 @@
+import mypack2.*;
+// import mypack2.UserDefinedPackageDemo2;
+
+ class UserDefinedPackageDemo3 {
+      public static void main(String[] args){
+         UserDefinedPackageDemo2 obj = new UserDefinedPackageDemo2(10,20);     
+         System.out.println(obj);
+      }
+}
---
+++ Packages_in_Java/mypack2/UserDefinedPackageDemo2.java
@@ -0,0 +1,16 @@
+package Packages_in_Java.mypack2;
+
+public class UserDefinedPackageDemo2 {
+      int i;
+      int j;
+
+      public UserDefinedPackageDemo2(int i, int j){
+            this.i = i;
+            this.j = j;
+      }
+
+      public String toString() {
+            return "I: " + i + " J: " + j;
+      }
+      
+}
